01dbb78d8e5d48b7b15bca6ad31090a8f70491fe
tonmoy <tonmoyhalder50@gmail.com>
2023-11-19 19:10:52 -0500

    updated problem4

--- Fall 2023/B503/Homework4/Problem4.py
+++ Fall 2023/B503/Homework4/Problem4.py
@@ -1,32 +1,26 @@
-import math
+# Hoy nai
 
 
-def calculate_grundy_numbers(n):
-    grundy_numbers = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        factors = set()
-        for j in range(1, int(math.sqrt(i)) + 1):
-            if i % j == 0:
-                factors.add(grundy_numbers[j])
-                factors.add(grundy_numbers[i // j])
-
-        mex = 0
-        while mex in factors:
-            mex += 1
-
-        grundy_numbers[i] = mex
-
-    return grundy_numbers
+def find_moves(A, current_position):
+    for i in range(len(A)):
+        if (i - current_position) % A[current_position] == 0 and A[i] > A[
+            current_position
+        ]:
+            yield i
 
 
-def who_wins(n):
-    grundy_numbers = calculate_grundy_numbers(n)
-    winners = ["Joe" if grundy_numbers[i] != 0 else "Jane" for i in range(1, n + 1)]
+def determine_winner(A):
+    winners = [""] * len(A)
+    for i in range(len(A)):
+        if len(list(find_moves(A, i))) % 2 == 0:
+            winners[i] = "Joe"
+        else:
+            winners[i] = "Jane"
+
     return winners
 
 
-# Example usage:
-n = 5
-winners = who_wins(n)
-print("Winners:", winners)
+if __name__ == "__main__":
+    A = [6, 5, 7, 1, 4, 3]
+    # A = [1, 2, 3, 4, 5, 6, 7, 9]
+    print(determine_winner(A))
